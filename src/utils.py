92c02e1e02c3f685f0783c1f45e4663f0387eb15
Pavel Shkolyansky <sprain@yandex.ru>
2021-06-21 14:48:08 +0300

    refactoring code

--- src/utils.py
+++ src/utils.py
@@ -2,39 +2,63 @@ import numpy as np
 import pandas as pd
 
 
-def prefilter_items(data):
+def prefilter_items(data, take_n_popular=5000, item_features=None):
 
-    # исключим из данных продажи с нулевым "quantity"
-    data = data[data['quantity'] >= 1]
+    set_of_excluded_items = set()
     
-    # исключим из данных продажи с нулевым "sales_value"
-    data = data[data['sales_value'] > 0]
+    # определяем все продажи с нулевым "quantity"
+    products_quantity = data.groupby('item_id')['quantity'].sum().reset_index()
+    zero_quantity_sales_items = products_quantity[products_quantity['quantity'] < 1]['item_id'].unique().tolist()
+    set_of_excluded_items.update(zero_quantity_sales_items)
     
-    popularity = data.groupby('item_id')['user_id'].nunique().reset_index()
-    popularity.rename(columns={'user_id': 'share_unique_users'}, inplace=True)
-    popularity['share_unique_users'] = popularity['share_unique_users'] / data['user_id'].nunique()
+    # определим товары, которые не продавались за последние 12 месяцев (последние 52 недели)
+    old_item = list(set(data[data['week_no'] < data['week_no'].max() - 52].item_id.unique().tolist())
+                    - set(data[data['week_no'] >= data['week_no'].max() - 52].item_id.unique().tolist()))
+    set_of_excluded_items.update(old_item)
+    
+    # определим слишком дешевые товары (на них не заработаем). 1 покупка из рассылок стоит 60 руб.
+    # и слишком дорогие товары
+    products_price = data[['item_id', 'quantity', 'sales_value']].copy()
+
+    products_price['price'] = products_price['sales_value'] / np.maximum(products_price['quantity'], 1)
+    products_price = products_price.groupby('item_id')['price'].mean().reset_index()
+
+    noninterest_items = products_price[~products_price['price'].between(2, 100)].item_id.unique().tolist()
+    set_of_excluded_items.update(noninterest_items)
     
     # Уберем самые популярные товары (их и так купят), а также
     # самые НЕ популярные товары (их и так НЕ купят)
-    popular = popularity[popularity['share_unique_users'].between(.03, .5)].item_id.tolist()
-    data = data[~data['item_id'].isin(popular)]
+    popular_items = data[~data['item_id'].isin(set_of_excluded_items)].groupby('item_id')['user_id'].nunique().reset_index()
+    popular_items.rename(columns={'user_id': 'n_users'}, inplace=True)
+    popular_items['n_users'] = popular_items['n_users'] / data[~data['item_id'].isin(set_of_excluded_items)]['user_id'].nunique()
     
-    # Уберем товары, которые не продавались за последние 12 месяцев (последние 52 недели)
-    old_item = list(set(data[data['week_no'] < data['week_no'].max() - 52].item_id.unique().tolist())\
-                    - set(data[data['week_no'] >= data['week_no'].max() - 52].item_id.unique().tolist()))
-    data = data[~data['item_id'].isin(old_item)] # 140 позиций
+    nonpopular_items = popular_items[popular_items['n_users'].between(.03, .4)].item_id.tolist()
+    set_of_excluded_items.update(nonpopular_items)
     
-    # Уберем не интересные для рекоммендаций категории (department)
-    # пока ничего не будем убирать    
+    # # Уберем не интересные для рекоммендаций категории (department)
+    # if item_features is not None:
+    #     department_size = pd.DataFrame(item_features. \
+    #                                    groupby('department')['item_id'].nunique(). \
+    #                                    sort_values(ascending=False)).reset_index()
+
+    #     department_size.columns = ['department', 'n_items']
+    #     rare_departments = department_size[department_size['n_items'] < 150].department.tolist()
+    #     items_in_rare_departments = item_features[
+    #         item_features['department'].isin(rare_departments)].item_id.unique().tolist()
+
+    #     data = data[~data['item_id'].isin(items_in_rare_departments)]
     
-#     data['price'] = data['sales_value'] / (np.maximum(data['quantity'], 1))
-    # Уберем слишком дешевые товары (на них не заработаем). 1 покупка из рассылок стоит 60 руб.
-    # И слишком дорогие товары
-    products_price = data[['item_id', 'quantity', 'sales_value']]
-    products_price['price'] = products_price['sales_value'] / products_price['quantity']
-    products_price = products_price.groupby('item_id')['price'].mean().reset_index()
-    interest_items = products_price[products_price['price'].between(1, 150)].item_id.unique().tolist()
-    data = data[data['item_id'].isin(interest_items)]
+    # Возбмем топ по популярности
+    popularity = data[~data['item_id'].isin(set_of_excluded_items)].groupby('item_id')['user_id'].sum().reset_index()
+    popularity.rename(columns={'user_id': 'n_users'}, inplace=True)
+
+    nontop_items = popularity.sort_values('n_users', ascending=False)[take_n_popular:].item_id.tolist()
+    set_of_excluded_items.update(nontop_items)
+    
+    # data[~data['item_id'].isin(set_of_excluded_items)]['item_id'].nunique() == 5000 => True
+    
+    # Заведем фиктивный item_id (если юзер покупал товары не из топ-5000, то он "купил" такой товар)
+    data.loc[data['item_id'].isin(set_of_excluded_items), 'item_id'] = 999999
     
     # ...
     return data
